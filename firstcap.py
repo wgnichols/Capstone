fde34e1bb54ceebac2c48b9912bd913a1a7d4ad5
wgnichols <64288803+wgnichols@users.noreply.github.com>
2020-05-04 16:22:35 -0500

    Add files via upload
    First assignment for the capstone project

---
+++ firstcap.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# This notebook serves as a workspace for the final Capstone project. I will be sharing and updating this regularly until the assignment is due.
+
+# In[1]:
+
+
+import pandas as pd
+
+
+# In[2]:
+
+
+import numpy as np
+
+
+# In[3]:
+
+
+print('Hello Capstone Project Course!')
+
+
+# In[ ]:
+
+
+
+
